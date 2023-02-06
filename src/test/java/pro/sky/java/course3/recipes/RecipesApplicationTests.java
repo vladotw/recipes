12e006d5ca16868d39f877ca73831a1ce160fc6a
Vlad <vladot@ya.ru>
2023-02-06 17:47:59 +0600

    Initial commit

---
+++ src/test/java/pro/sky/java/course3/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
